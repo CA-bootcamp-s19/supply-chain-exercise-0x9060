133f07472d8bdb12555efb6475e63ff08a7acbec
Clark <59158959+0x9060@users.noreply.github.com>
2020-09-26 10:23:55 -0700

    Unit tests (#7)
    * Starting to make solidity tests - logic doesn't quite make sense yet
    
    * Cleaning up keywords for test function.
    
    * ThrowProxy compiling. Need to test actual throws much further
    
    * Implementing changes from master (SupplyChain contract)
    
    Co-authored-by: Clark <clark@bytecache.io>

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,15 +5,57 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
+// Proxy for testing Throws
+contract ThrowProxy {
+    address public target;
+    bytes data;
+
+    constructor(address _target) public {
+        target = _target;
+    }
+
+    // prime the data using fallback
+    function() external {
+        data = msg.data;
+    }
+
+    function execute() public returns(bool) {
+        (bool ret, ) = target.call(data);
+        return ret;
+    }
+}
+
 contract TestSupplyChain {
 
     // Test for failing conditions in this contracts:
     // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
 
+    SupplyChain public supplyChain;
+    ThrowProxy public throwproxy;
+
+    // Run before each test function
+    function beforeEach() public {
+        throwProxy = new ThrowProxy(address(supplyChain));
+
+        supplyChain = new SupplyChain();
+        supplyChain.addItem("widget", 1000);
+    }
+
     // buyItem
 
     // test for failure if user does not send enough funds
+    function testBuyerSendsNotEnoughFunds() public payable {
+
+        // prime the proxy
+        SupplyChain(address(throwProxy)).buyItem(0);
+
+        bool r = throwProxy.execute.gas(200000)();
+
+        Assert.isTrue(r, "buyer should send enough funds");
+    }
+
     // test for purchasing an item that is not for Sale
+    // function testPurchasedItemNotForSale() public {}
 
     // shipItem
 
