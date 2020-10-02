1e19fa2ce29617b473a548e83589ce758f02aecb
Clark <clark@bytecache.io>
2020-10-01 21:12:18 -0700

    ThrowProxy working for some function!

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,6 +5,108 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
+/// TESTING
+
+contract TestSupplyChain {
+
+    // Test for failing conditions in this contracts:
+    // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
+
+    uint public initialBalance = 2 ether;
+    SupplyChain supplyChain;
+    ThrowProxy throwProxy;
+    UserAgent seller;
+    UserAgent buyer;
+
+    // Run before each test function
+    function beforeEach() public {
+        supplyChain = new SupplyChain();
+        throwProxy = new ThrowProxy(address(supplyChain));
+        seller = new  UserAgent();
+        buyer = (new UserAgent).value(1000)();
+
+        // Common operations
+        //SupplyChain(address(throwProxy)).addItem("widget2", 50 wei);
+        supplyChain.addItem("widget2", 50 wei);
+        seller.addItem(supplyChain, "widget", 99 wei);
+        buyer.fetchItem(supplyChain, 0);
+    }
+
+    // buyItem
+
+    // test for failure if user does not send enough funds
+    function testBuyerSendsNotEnoughFunds() public payable {
+
+        //seller.addItem(supplyChain, "butter", 888 wei);
+        //buyer.fetchItem(supplyChain, 1);
+
+        //buyer.buyItem(supplyChain, 0, 100 wei);
+        //buyer.buyItem(supplyChain, 0, 100 wei);
+        //Assert.equal(uint(5), uint(4), "Stop");
+
+        //Assert.isFalse(r, "buyer should send enough funds");
+    }
+
+    // test for purchasing an item that is not for Sale
+    function testPurchasedItemNotForSale() public {
+        // seller.addItem(supplyChain, "butter", 888 wei);
+        // buyer.buyItem(supplyChain, 0, 100 wei);
+        //buyer.buyItem(supplyChain, 0, 100 wei);
+        //supplyChain.buyItem(4);
+        SupplyChain(address(throwProxy)).buyItem(4);
+
+        bool r = throwProxy.execute.gas(2000000)();
+
+        //Assert.isFalse(r, "Should be false");
+        Assert.isFalse(r, "Should be false");
+    }
+
+    // shipItem
+
+    // test for calls that are made by not the seller
+    // test for trying to ship an item that is not marked Sold
+
+    // receiveItem
+
+    // test calling the function from an address that is not the buyer
+    // test calling the function on an item not marked Shipped
+
+}
+
+
+/// Helper Functions
+
+// Fake Contract User
+contract UserAgent {
+
+    event LogFetchedPrice(uint indexed price);
+
+    constructor() public payable {}
+
+    function addItem(SupplyChain _supplyChain, string memory _item, uint _price) public {
+        _supplyChain.addItem(_item, _price);
+    }
+
+    function shipItem(SupplyChain _supplyChain, uint _sku) public {
+        _supplyChain.shipItem(_sku);
+    }
+
+    function buyItem(SupplyChain _supplyChain, uint _sku, uint amount) public {
+        _supplyChain.buyItem.value(amount)(_sku);
+    }
+
+    function receiveItem(SupplyChain _supplyChain, uint _sku) public {
+        _supplyChain.receiveItem(_sku);
+    }
+
+    function fetchItem(SupplyChain _supplyChain, uint _sku) public {
+        (,,uint price,,,) = _supplyChain.fetchItem(_sku);
+
+        emit LogFetchedPrice(price);
+    }
+
+}
+
 // Proxy for testing Throws
 contract ThrowProxy {
     address public target;
@@ -23,48 +125,4 @@ contract ThrowProxy {
         (bool ret, ) = target.call(data);
         return ret;
     }
-}
-
-contract TestSupplyChain {
-
-    // Test for failing conditions in this contracts:
-    // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
-
-    SupplyChain public supplyChain;
-    ThrowProxy public throwproxy;
-
-    // Run before each test function
-    function beforeEach() public {
-        throwProxy = new ThrowProxy(address(supplyChain));
-
-        supplyChain = new SupplyChain();
-        supplyChain.addItem("widget", 1000);
-    }
-
-    // buyItem
-
-    // test for failure if user does not send enough funds
-    function testBuyerSendsNotEnoughFunds() public payable {
-
-        // prime the proxy
-        SupplyChain(address(throwProxy)).buyItem(0);
-
-        bool r = throwProxy.execute.gas(200000)();
-
-        Assert.isTrue(r, "buyer should send enough funds");
-    }
-
-    // test for purchasing an item that is not for Sale
-    // function testPurchasedItemNotForSale() public {}
-
-    // shipItem
-
-    // test for calls that are made by not the seller
-    // test for trying to ship an item that is not marked Sold
-
-    // receiveItem
-
-    // test calling the function from an address that is not the buyer
-    // test calling the function on an item not marked Shipped
-
 }
